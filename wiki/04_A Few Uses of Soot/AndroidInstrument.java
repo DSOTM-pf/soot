77bd3d7eb0decbed39b90ba4ba138bd824549c91
wowhhh <1792195840@qq.com>
2019-11-14 00:19:56 +0800

    android code

--- wiki/04_A Few Uses of Soot/AndroidInstrument.java
+++ wiki/04_A Few Uses of Soot/AndroidInstrument.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -12,108 +13,98 @@ import soot.SootClass;
 import soot.SootMethod;
 import soot.Transform;
 import soot.Unit;
-import soot.jimple.AbstractStmtSwitch;
-import soot.jimple.InvokeExpr;
-import soot.jimple.InvokeStmt;
-import soot.jimple.Jimple;
-import soot.jimple.StringConstant;
+import soot.jimple.*;
 import soot.options.Options;
 
 
 public class AndroidInstrument {
 
-	public static void main(String[] args) {
+        public static void main(String[] args) {
 
-		//prefer Android APK files// -src-prec apk
-    /**设置处理的文件类型，Options.src_prec_apk表示处理的是Android apk*/
-		Options.v().set_src_prec(Options.src_prec_apk);
-
-		//output as APK, too//-f J
-    ////设置输出的文件类型:output_format_dex表示输出的文件的apk/dex，将会输出在工程目录下的sootOutput目录
-		Options.v().set_output_format(Options.output_format_dex);
-
-    //设置Soot的soot-class路径
-    Options.v().set_soot_classpath("G:\\Soot\\sdk\\platforms\\android-17\\android.jar;C:\\Program Files\\Java\\jre7\\lib\\rt.jar;C:\\Program Files\\Java\\jre7\\lib\\jce.jar;G:\\李政桥\\工具\\android相关工具\\soot-trunk.jar");
+            Options.v().set_allow_phantom_refs(true);
+            Options.v().set_validate(true);
 
 
-        // resolve the PrintStream and System soot-classes
+            /**output_format_dex表示输出文件的apk/dex，将输出在sootOutPut目录下*/
+            Options.v().set_output_format(Options.output_format_dex);
+            Options.v().set_process_dir(Collections.singletonList("D:\\soottest\\utils\\Soot.apk"));
+            Options.v().set_force_android_jar("D:\\soottest\\utils\\android_25\\android.jar");
 
-    	//加载java.io.PrintStream和java.lang.System
-    	//java.lang.System中存在字段static PrintStream out
-    	//java.io.PrintStream中存在方法void println(java.lang.String)
-    	//插入的代码是：System.out.println("HELLO");
-		Scene.v().addBasicClass("java.io.PrintStream",SootClass.SIGNATURES);
-        Scene.v().addBasicClass("java.lang.System",SootClass.SIGNATURES);
+            /**设置处理文件的类型，src_prec_apk表示处理的是apk文件*/
+            Options.v().set_src_prec(Options.src_prec_apk);
+            /**设置Soot的soot-class*/
+            Options.v().set_soot_classpath("D:\\soottest\\utils\\android_25\\android.jar;C:\\Program Files\\Java\\jdk1.8.0_181\\jre\\lib\\rt.jar;C:\\Program Files\\Java\\jdk1.8.0_181\\jre\\lib\\jce.jar;" +
+                    "D:\\soottest\\utils\\sootclasses-trunk-jar-with-dependencies.jar");
 
-        PackManager.v().getPack("jtp").add(new Transform("jtp.myInstrumenter", new BodyTransformer() {
+            Scene.v().loadNecessaryClasses();
+            // resolve the PrintStream and System soot-classes
+            Scene.v().addBasicClass("java.io.PrintStream",SootClass.SIGNATURES);
+            Scene.v().addBasicClass("java.lang.System",SootClass.SIGNATURES);
 
-			@Override
-			protected void internalTransform(final Body b, String phaseName, @SuppressWarnings("rawtypes") Map options) {
-        //Body带代表一个method body；
-        //Body中存在三个基本元素：locals、statements、exceptions，即：getLocals();getUnits();getTraps()
-      	final PatchingChain<Unit> units = b.getUnits();
+            PackManager.v().getPack("jtp").add(new Transform("jtp.myInstrumenter", new BodyTransformer() {
 
-				//important to use snapshotIterator here
-				for(Iterator<Unit> iter = units.snapshotIterator(); iter.hasNext();) {
-					final Unit u = iter.next();
-					u.apply(new AbstractStmtSwitch() {
-            //如果u是一个调用语句的时候，类似：staticinvoke <com.example.source_a.MainActivity: int add4(int,int)>($i0, $i1);
-						public void caseInvokeStmt(InvokeStmt stmt) {
-							InvokeExpr invokeExpr = stmt.getInvokeExpr();
-              //该调用语句中，如果被调用的方法是onDraw时
-							if(invokeExpr.getMethod().getName().equals("onDraw")) {
+                @Override
+                protected void internalTransform(final Body b, String phaseName, @SuppressWarnings("rawtypes") Map options) {
+                    final PatchingChain<Unit> units = b.getUnits();
 
-								Local tmpRef = addTmpRef(b);
-								Local tmpString = addTmpString(b);
-                //插入：tmpRef = <java.lang.System: java.io.PrintStream out>;类似于类的实例化
-							 //newAssignStmt()为赋值语句
-								  // insert "tmpRef = java.lang.System.out;"
-						        units.insertBefore(Jimple.v().newAssignStmt(
-						                      tmpRef, Jimple.v().newStaticFieldRef(
-						                      Scene.v().getField("<java.lang.System: java.io.PrintStream out>").makeRef())), u);
+                    //important to use snapshotIterator here
+                    for(Iterator<Unit> iter = units.snapshotIterator(); iter.hasNext();) {
+                        final Unit u = iter.next();
+                        u.apply(new AbstractStmtSwitch() {
+                            //如果u是一个调用语句的时候，类似：staticinvoke <com.example.source_a.MainActivity: int add4(int,int)>($i0, $i1)
+                            public void caseInvokeStmt(InvokeStmt stmt) {
+                                //该调用语句中，如果被调用的方法是add时
+                                InvokeExpr invokeExpr = stmt.getInvokeExpr();
+                                if(invokeExpr.getMethod().getName().equals("add")) {
 
-						        // insert "tmpLong = 'HELLO';"
-                    //插入：tmpLong = "HELLO"
-						        units.insertBefore(Jimple.v().newAssignStmt(tmpString,
-						                      StringConstant.v("HELLO")), u);
+                                    Local tmpRef = addTmpRef(b);
+                                    Local tmpString = addTmpString(b);
+                                    //插入：tmpRef = <java.lang.System: java.io.PrintStream out>;类似于类的实例化
+                                    //newAssignStmt()为赋值语句
+                                    // insert "tmpRef = java.lang.System.out;"
+                                    units.insertBefore(Jimple.v().newAssignStmt(
+                                            tmpRef, Jimple.v().newStaticFieldRef(
+                                                    Scene.v().getField("<java.lang.System: java.io.PrintStream out>").makeRef())), u);
 
-						        // insert "tmpRef.println(tmpString);"
-						        SootMethod toCall = Scene.v().getSootClass("java.io.PrintStream").getMethod("void println(java.lang.String)");
-                    //newVirtualInvokeExpr生成调用语句 第1个参数为类的实例化，第2个参数为类中的方法的引用，第3个为方法的参数
-                    units.insertBefore(Jimple.v().newInvokeStmt(
-						                      Jimple.v().newVirtualInvokeExpr(tmpRef, toCall.makeRef(), tmpString)), u);
+                                    // insert "tmpLong = 'HELLO';"
+                                    units.insertBefore(Jimple.v().newAssignStmt(tmpString,
+                                            StringConstant.v("HELLO")), u);
 
-						        //check that we did not mess up the Jimple
-						        b.validate();
-							}
-						}
-            //同样可以处理其他语句,如下面的赋值语句
-					public void caseAssignStmt(AssignStmt stmt){}
-					//这些都是class AbstraceStmtSwitch中可以查看
-					});
-				}
-			}
+                                    // insert "tmpRef.println(tmpString);"
+                                    SootMethod toCall = Scene.v().getSootClass("java.io.PrintStream").getMethod("void println(java.lang.String)");
+                                    units.insertBefore(Jimple.v().newInvokeStmt(
+                                            Jimple.v().newVirtualInvokeExpr(tmpRef, toCall.makeRef(), tmpString)), u);
+
+                                    //check that we did not mess up the Jimple
+                                    b.validate();
+                                }
+                            }
+                            //同样可以处理其他语句,如下面的赋值语句
+                            public void caseAssignStmt(AssignStmt stmt){}
+                            //这些都是class AbstraceStmtSwitch中可以查看
+
+                        });
+                    }
+                }
 
 
-		}));
+            }));
+            PackManager.v().runPacks();
+            PackManager.v().writeOutput();
+            soot.Main.main(args);
+        }
 
-		soot.Main.main(args);
-	}
+        private static Local addTmpRef(Body body)
+        {
+            Local tmpRef = Jimple.v().newLocal("tmpRef", RefType.v("java.io.PrintStream"));
+            body.getLocals().add(tmpRef);
+            return tmpRef;
+        }
 
-    private static Local addTmpRef(Body body)
-    {
-      //生成Local tmpRef,生成的Jimple代码为：java.io.PrintStrem tmpRef;
-        Local tmpRef = Jimple.v().newLocal("tmpRef", RefType.v("java.io.PrintStream"));
-        //加到Locals链中
-        body.getLocals().add(tmpRef);
-        return tmpRef;
-    }
-
-    private static Local addTmpString(Body body)
-    {
-      //生成Local tmpString，生成的Jimple代码为：java.lang.String tmpString;
-        Local tmpString = Jimple.v().newLocal("tmpString", RefType.v("java.lang.String"));
-        body.getLocals().add(tmpString);
-        return tmpString;
-    }
+        private static Local addTmpString(Body body)
+        {
+            Local tmpString = Jimple.v().newLocal("tmpString", RefType.v("java.lang.String"));
+            body.getLocals().add(tmpString);
+            return tmpString;
+        }
 }
